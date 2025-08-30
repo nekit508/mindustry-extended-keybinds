d7cfb211637a13777a92677b28138a090a90c386
nekit508 <lastp508@gmail.com>
2025-08-30 11:02:16 +0300

    dialog

--- src/com/github/nekit508/emkb/core/EMKBCore.java
+++ src/com/github/nekit508/emkb/core/EMKBCore.java
@@ -1,6 +1,13 @@
 package com.github.nekit508.emkb.core;
 
+import com.github.nekit508.emkb.ui.dialogs.KeybindsDialog;
 import mindustry.mod.Mod;
 
 public class EMKBCore extends Mod {
+    public static KeybindsDialog dialog;
+
+    @Override
+    public void init() {
+        dialog = new KeybindsDialog("emkb-keybinds-dialog");
+    }
 }
